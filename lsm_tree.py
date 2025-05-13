90fe0992a8591d90f2eedbe9416cc1132c09d099
alan.huang <alan.huang@psp-power.com.tw>
2025-05-13 15:19:24 +0800

    feat: add SSTable class and enhance LSMTree with flush functionality

--- lsm_tree.py
+++ lsm_tree.py
@@ -1,9 +1,12 @@
+from sstable import SSTable
+
 class LSMTree:
     def __init__(self, flush_threshold):
         self.flush_threshold = flush_threshold
         self.mem_table = {}
         self.sstables = []
-        
+        self.last_sstable_id = 0
+
     def write(self, key, value):
         self.mem_table[key] = value
         if len(self.mem_table) > self.flush_threshold:
@@ -13,9 +16,17 @@ class LSMTree:
         if key in self.mem_table:
             return self.mem_table[key]
 
-        # TODO: read from sstables
+        for sstable in self.sstables[::-1]:
+            data = sstable.read_key(key)
+            if data is not None:
+                return data
         return None
 
     def flush(self):
-        # TODO
-        return
+        filename = f'sstable_{self.last_sstable_id}.txt'
+        new_sstable = SSTable(filename)
+        new_sstable.save(self.mem_table)
+        self.sstables.append(new_sstable)
+        self.mem_table = {}
+        self.last_sstable_id += 1
+
---
+++ sstable.py
@@ -0,0 +1,26 @@
+import json
+
+class SSTable:
+    def __init__(self, filename: str):
+        self.filename = filename
+
+    def read_key(self, key: str):
+        with open(self.filename) as f:
+            for line in f:
+                record = json.loads(line.strip())
+                if record['key'] == key:
+                    return record['value']
+        return None
+
+    def read_records(self):
+        with open(self.filename) as f:
+            for line in f:
+                record = json.loads(line.strip())
+                yield record
+
+    def save(self, mem_table: dict) -> None:
+        with open(self.filename, 'w') as f:
+            for key, value in sorted(mem_table.items()):
+                dump_value = json.dumps({'key': key, 'value': value})
+                f.write(dump_value + '\n')
+
