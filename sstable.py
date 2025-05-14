ba01b604a0dca13b792392a2566eb4fc56353af7
alan.huang <alan.huang@psp-power.com.tw>
2025-05-14 14:49:31 +0800

    style: improve code consistency in sstable.py by standardizing quotes and formatting

--- sstable.py
+++ sstable.py
@@ -1,5 +1,6 @@
 import json
 
+
 class SSTable:
     def __init__(self, filename: str):
         self.filename = filename
@@ -8,8 +9,8 @@ class SSTable:
         with open(self.filename) as f:
             for line in f:
                 record = json.loads(line.strip())
-                if record['key'] == key:
-                    return record['value']
+                if record["key"] == key:
+                    return record["value"]
         return None
 
     def read_records(self):
@@ -19,8 +20,7 @@ class SSTable:
                 yield record
 
     def save(self, mem_table: dict) -> None:
-        with open(self.filename, 'w') as f:
+        with open(self.filename, "w") as f:
             for key, value in sorted(mem_table.items()):
-                dump_value = json.dumps({'key': key, 'value': value})
-                f.write(dump_value + '\n')
-
+                dump_value = json.dumps({"key": key, "value": value})
+                f.write(dump_value + "\n")
