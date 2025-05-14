ad4ddccb57e23635c1c9ac33863c03d1a2083110
alan.huang <alan.huang@psp-power.com.tw>
2025-05-14 10:35:16 +0800

    feat: add read_range method to LSMTree for querying key ranges

--- lsm_tree.py
+++ lsm_tree.py
@@ -23,6 +23,22 @@ class LSMTree:
                 return data
         return None
 
+    def read_range(self, start_key, end_key):
+        result = {}
+        for key in self.mem_table:
+            if start_key <= key <= end_key:
+                result[key] = self.mem_table[key]
+
+        for sstable in self.sstables[::-1]:
+            for record in sstable.read_records():
+                key, value = record['key'], record['value']
+                if record['key'] in result:
+                    continue
+                else:
+                    if start_key <= key <= end_key:
+                        result[key] = value
+        return result
+
     def flush(self):
         filename = f'sstable_{self.last_sstable_id}.txt'
         new_sstable = SSTable(filename)
@@ -38,8 +54,7 @@ class LSMTree:
         merged = {}
         for sstable in self.sstables:
             for record in sstable.read_records():
-                key = record['key']
-                value = record['value']
+                key, value = record['key'], record['value']
                 merged[key] = value
 
         self.sstables = []
